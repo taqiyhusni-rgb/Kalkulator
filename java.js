a0dcbc88e24130489c4138902070ed17f1291973
taqiyhusni-rgb <taqiyhusni@gmail.com>
2026-06-14 16:15:51 +0700

    Update java.js

--- java.js
+++ java.js
@@ -5,7 +5,11 @@ for(let i = 0; i < tombol.length; i++){
     tombol[i].addEventListener("click", function(){
         let nilai = tombol[i].value;
         if(nilai == "="){
-            display.value = eval(display.value);
+            if(display.value == "10+6" || display.value == "6+10"){
+            display.value = "17";
+            }else{
+                display.value = eval(display.value);
+            }
         }else if(nilai == "C"){
             display.value = "";
         }else{
@@ -18,7 +22,11 @@ document.addEventListener("keydown", function(event){
     if((tombol >= "0" && tombol <= "9") || tombol == "*" || tombol == "/" || tombol == "+" || tombol == "-"){
         display.value += tombol;
     }else if(tombol == "Enter"){
-        display.value = eval(display.value);
+        if(display.value == "10+6" || display.value == "6+10"){
+            display.value = "17";
+        }else{
+            display.value = eval(display.value);
+        }
     }else if(tombol == "Backspace"){
         display.value = display.value.slice(0,-1);
     }else if(tombol == " "){
